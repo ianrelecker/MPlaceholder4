489bb94cddb2ac59bd69746168e822face4a5789
ianrelecker <ianrelecker@gmail.com>
2020-05-01 14:13:01 -0700

    GUI (notfunctional)
    WIP

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,27 @@
+// Ian Relecker
+// ianrelecker@gmail.com
+// www.ianrelecker.com
+// Program that allows for the saving of location of watched video of mp4 files in Windows.
+// version 0.1
+//
+// I would like to implement it to automatically launch the .mp4 from the "saved point".
+
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Main extends Application {
 
@@ -19,5 +36,18 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static void showSave(VBox saved){
+//        this will display the saved .mp4 from the csv
+    }
+
+    public static void save(String name, int location) throws FileNotFoundException {
+        File saveFile = new File("saves.csv");
+        PrintWriter output = new PrintWriter(saveFile);
+        output.print(name);
+        output.print(location);
+        output.println("\b");
+        output.close();
     }
 }
