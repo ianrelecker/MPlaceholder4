fb000c3fca7f0d0d41fff981331652b43f37be65
ianrelecker <ianrelecker@gmail.com>
2020-05-01 16:11:10 -0700

    WIP
    V 0.2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,7 +2,7 @@
 // ianrelecker@gmail.com
 // www.ianrelecker.com
 // Program that allows for the saving of location of watched video of mp4 files in Windows.
-// version 0.1
+// version 0.2
 //
 // I would like to implement it to automatically launch the .mp4 from the "saved point".
 
@@ -19,15 +19,16 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("MPlaceholder4");
+        HBox saved = new HBox(20);
 
 //
         TextField userInputPath = new TextField();
@@ -42,10 +43,17 @@ public class Main extends Application {
 
 //
         Button enter = new Button("Save");
+        enter.setOnAction(e -> {
+            try {
+                save(userInputPath.getText(), userInputLocation.getText());
+            } catch (IOException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            }
+            saved.getChildren();
+        });
 
 
-//
-
+// Layout stuff
         VBox vbox12 = new VBox(0);
         HBox hbox1 = new HBox(20);
         hbox1.getChildren().addAll(userInputPath);
@@ -59,20 +67,19 @@ public class Main extends Application {
         hbox3.setAlignment(Pos.CENTER);
         Label padd = new Label(" ");
         Label padd2 = new Label("");
-//
 
-        vbox12.getChildren().addAll(labelP, hbox1, padd, labelL, hbox2, padd2, hbox3);
+
+// layout for showing the saved items
+        try {
+            showSave(saved);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+// final layout
+        vbox12.getChildren().addAll(labelP, hbox1, padd, labelL, hbox2, padd2, hbox3, saved);
         vbox12.setPadding(new Insets(20,20,20,20));
-
-
-
-
-
-        VBox saved = new VBox(20);
-        showSave(saved);
-
-
-
 
         Scene layout1 = new Scene(vbox12, 300, 500);
         primaryStage.setScene(layout1);
@@ -84,16 +91,63 @@ public class Main extends Application {
         launch(args);
     }
 
-    public static void showSave(VBox saved){
-//        this will display the saved .mp4 from the csv
+    public static void showSave(HBox saved) throws FileNotFoundException, NoSuchElementException {
+        File saveFile = new File("saves.csv");
+        Scanner fileIn = new Scanner(saveFile);
+        int amtLines = 1;
+        int exit = 0;
+        while (exit == 0){
+            try{
+            if(fileIn.nextLine() != null){
+                exit++;
+            }
+            amtLines++;
+            }catch (NoSuchElementException p){
+                p.getStackTrace();
+                break;
+
+            }
+        }
+//
+        Scanner fileIn1 = new Scanner(saveFile);
+        String[] paths = new String[amtLines];
+        String[] locations = new String[amtLines];
+        int counter = 0;
+        while (counter < amtLines){
+            try {
+                if (counter % 2 == 0) {
+                    paths[counter] = fileIn1.nextLine();
+
+                } else {
+                    locations[counter] = fileIn1.nextLine();
+                }
+                counter++;
+            }catch(NoSuchElementException o){
+                o.getStackTrace();
+                break;
+            }
+        }
+//
+        VBox pathsBox = new VBox(0);
+        VBox locateBox = new VBox(0);
+        for(int i=0; i<paths.length; i++){
+            Label tempLabel = new Label(paths[i]);
+            pathsBox.getChildren().add(i, tempLabel);
+        }
+        for(int i=0; i<locations.length; i++){
+            Label tempLabel2 = new Label(locations[i]);
+            locateBox.getChildren().add(i, tempLabel2);
+        }
+        pathsBox.setAlignment(Pos.CENTER);
+        locateBox.setAlignment(Pos.CENTER);
+        saved.getChildren().addAll(pathsBox, locateBox);
     }
 
-    public static void save(String name, int location) throws FileNotFoundException {
+    public void save(String path, String location) throws IOException {
         File saveFile = new File("saves.csv");
-        PrintWriter output = new PrintWriter(saveFile);
-        output.print(name);
-        output.print(location);
-        output.println("\b");
+        FileWriter output = new FileWriter(saveFile, true);
+        output.write(path + "\n");
+        output.append(location).append("\n");
         output.close();
     }
 }
